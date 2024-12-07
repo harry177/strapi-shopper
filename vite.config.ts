ef89f64b261205b87cbb331e11f90f524d41bb97
Artem Prygunov <harry177@rambler.ru>
2024-12-08 00:59:52 +0100

    feat: add PWA config and files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,48 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: "prompt",
+  includeAssets: [
+    "ss-android-chrome-192.png",
+    "ss-android-chrome-512.png",
+    "ss-ios-180.png",
+  ],
+  manifest: {
+    name: "strapi-shopper",
+    short_name: "strapi-shopper",
+    description: "Full-stack ecommerce PWA",
+    icons: [
+      {
+        src: "/ss-android-chrome-192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "favicon",
+      },
+      {
+        src: "/ss-android-chrome-512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "favicon",
+      },
+      {
+        src: "/ss-ios-180.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "apple touch icon",
+      },
+    ],
+    theme_color: "#171717",
+    background_color: "#f0e7db",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), VitePWA(manifestForPlugin)],
+});
