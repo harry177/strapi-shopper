bc9b6db064e9a48114ddf9d721544898016c905f
Artem Prygunov <harry177@rambler.ru>
2024-12-08 01:48:02 +0100

    feat: prepare production data

--- src/features/api/instance.ts
+++ src/features/api/instance.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-export const API_URL = "http://localhost:1337/api";
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://strapi-shopper.vercel.app/api"
+    : "http://localhost:1337/api";
 
 export const apiClient = axios.create({
   baseURL: API_URL,
